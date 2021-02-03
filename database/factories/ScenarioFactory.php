24a11ad06ecc8c845e8f7dc4979169abb1e5e31e
Funfare <info@funfare.de>
2021-02-03 15:49:34 +0000

    bla

---
+++ database/factories/ScenarioFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Scenario;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ScenarioFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Scenario::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
